9d9ccdf06ae44516a695a1c60d54174f42977875
bbbond123 <wangyibu123@gmail.com>
2022-05-30 13:29:19 +0800

    fix: upgrade react 18 version

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import ProductsPage from './pages/Products';
 import CartPage from './pages/Cart';
@@ -9,10 +9,12 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Switch>
-          <Route path="/" component={ProductsPage} exact />
-          <Route path="/cart" component={CartPage} exact />
-        </Switch>
+        <Routes>
+          <Route path="/">
+            <Route index element={<ProductsPage />} />
+            <Route path="cart" element={<CartPage />} />
+          </Route>
+        </Routes>
       </BrowserRouter>
     );
   }
