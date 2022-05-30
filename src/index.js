8ece4c00a7021d11f7f7af8820548b6a2a09e4d2
xiaoge_engineer <james@gcoder.io>
2022-05-30 13:12:01 +0800

    fix: upgrade support react 18

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
+import { createRoot } from 'react-dom/client';
+import { legacy_createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import reduxThunk from 'redux-thunk';
 
@@ -8,13 +8,13 @@ import './index.css';
 import App from './App';
 import shopReducer from './store/reducers';
 
-const store = createStore(shopReducer, applyMiddleware(reduxThunk));
+const store = legacy_createStore(shopReducer, applyMiddleware(reduxThunk));
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root'));
+root.render(
   <Provider store={store}>
     <App />
-  </Provider>,
-  document.getElementById('root')
+  </Provider>
 );
 
 // If you want your app to work offline and load faster, you can change
